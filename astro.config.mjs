1812c9735edf279bd30cdcea83c7bebefa82a93e
ChrisKoll <christian.kolland@web.de>
2025-09-12 13:52:32 +0200

    feat: disable image processing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService()
+  },
   integrations: [tailwind({
     config: {
       applyBaseStyles: false
